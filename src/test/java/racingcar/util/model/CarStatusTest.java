4df365999bf754b2785eafd8f6309c71a67eabd6
armd482 <armd482@naver.com>
2025-10-25 16:27:21 +0900

    test: CarStatus 객체 테스트

---
+++ src/test/java/racingcar/util/model/CarStatusTest.java
@@ -0,0 +1,16 @@
+package racingcar.util.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import racingcar.model.CarStatus;
+
+public class CarStatusTest {
+    @Test
+    @DisplayName("자동차 객체 초기화 테스트")
+    void carInitTest() {
+        CarStatus car = new CarStatus("pobi", 2);
+        assertThat("pobi").isEqualTo(car.name());
+        assertThat(2).isEqualTo(car.position());
+    }
+}
